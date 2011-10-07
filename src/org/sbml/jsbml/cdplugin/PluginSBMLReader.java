488b664aaf1362ffbb21188e4653c99b5f04f047
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2011-10-07 11:45:50 +0000

    Complemented the CellDesigner bridge for JSBML with the up-to-date version of CellDesigner, included license files and author's lists.

--- src/org/sbml/jsbml/cdplugin/PluginSBMLReader.java
+++ src/org/sbml/jsbml/cdplugin/PluginSBMLReader.java
@@ -1,31 +1,21 @@
 /*
  * $Id: PluginSBMLReader.java 181 2010-04-20 07:28:21Z andreas-draeger $
  * $URL: https://jsbml.svn.sourceforge.net/svnroot/jsbml/modules/CellDesigner/src/org/sbml/jsbml/PluginSBMLReader.java $
+ * ----------------------------------------------------------------------------
+ * This file is part of JSBML. Please visit <http://sbml.org/Software/JSBML>
+ * for the latest version of JSBML and more information about SBML.
  *
+ * Copyright (C) 2009-2011 jointly by the following organizations:
+ * 1. The University of Tuebingen, Germany
+ * 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 3. The California Institute of Technology, Pasadena, CA, USA
  *
- *==================================================================================
- * Copyright (c) 2009 the copyright is held jointly by the individual
- * authors. See the file AUTHORS for the list of authors.
- *
- * This file is part of jsbml, the pure java SBML library. Please visit
- * http://sbml.org for more information about SBML, and http://jsbml.sourceforge.net/
- * to get the latest version of jsbml.
- *
- * jsbml is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * jsbml is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with jsbml.  If not, see <http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>.
- *
- *===================================================================================
- *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as <http://sbml.org/Software/JSBML/License>.
+ * ----------------------------------------------------------------------------
  */
 package org.sbml.jsbml.cdplugin;
 
@@ -97,14 +87,15 @@ import org.sbml.jsbml.Unit;
 import org.sbml.jsbml.UnitDefinition;
 import org.sbml.jsbml.CVTerm.Qualifier;
 import org.sbml.jsbml.util.IOProgressListener;
-import org.sbml.jsbml.util.SBaseChangeListener;
+import org.sbml.jsbml.util.TreeNodeChangeListener;
 import org.sbml.libsbml.libsbml;
 import org.sbml.libsbml.libsbmlConstants;
 
 /**
  * 
  * @author Andreas Dr&auml;ger
- * 
+ * @version $Rev$
+ * @since 0.8
  */
 @SuppressWarnings("deprecation")
 public class PluginSBMLReader implements SBMLInputConverter {
@@ -124,7 +115,7 @@ public class PluginSBMLReader implements SBMLInputConverter {
 	/**
 	 * 
 	 */
-	protected LinkedList<SBaseChangeListener> listOfSBaseChangeListeners;
+	protected LinkedList<TreeNodeChangeListener> listOfTreeNodeChangeListeners;
 	/**
 	 * 
 	 */
@@ -173,7 +164,7 @@ public class PluginSBMLReader implements SBMLInputConverter {
 		super();
 		this.possibleEnzymes = possibleEnzymes;
 		this.setIOListeners = new HashSet<IOProgressListener>();
-		this.listOfSBaseChangeListeners = new LinkedList<SBaseChangeListener>();
+		this.listOfTreeNodeChangeListeners = new LinkedList<TreeNodeChangeListener>();
 	}
 
 	/**
@@ -181,8 +172,8 @@ public class PluginSBMLReader implements SBMLInputConverter {
 	 * @param sb
 	 */
 	public void addAllSBaseChangeListenersTo(SBase sb) {
-		for (SBaseChangeListener listener : listOfSBaseChangeListeners)
-			sb.addChangeListener(listener);
+		for (TreeNodeChangeListener listener : listOfTreeNodeChangeListeners)
+			sb.addTreeNodeChangeListener(listener);
 	}
 
 	/*
