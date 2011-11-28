b4e84499a7a2563c5c484196ba9ff2386ae2baf2
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2011-11-28 12:47:17 +0000

    Commit hat am Samstag wohl nicht geklappt weil sshgw1 down war.

--- src/org/sbml/jsbml/cdplugin/PluginChangeListener.java
+++ src/org/sbml/jsbml/cdplugin/PluginChangeListener.java
@@ -20,6 +20,19 @@ import java.beans.PropertyChangeEvent;
 
 import javax.swing.tree.TreeNode;
 
+import jp.sbi.celldesigner.plugin.CellDesignerPlugin;
+import jp.sbi.celldesigner.plugin.PluginCompartment;
+import jp.sbi.celldesigner.plugin.PluginCompartmentType;
+import jp.sbi.celldesigner.plugin.PluginFunctionDefinition;
+import jp.sbi.celldesigner.plugin.PluginModel;
+import jp.sbi.celldesigner.plugin.PluginParameter;
+import jp.sbi.celldesigner.plugin.PluginReaction;
+import jp.sbi.celldesigner.plugin.PluginSimpleSpeciesReference;
+import jp.sbi.celldesigner.plugin.PluginSpecies;
+import jp.sbi.celldesigner.plugin.PluginSpeciesType;
+import jp.sbi.celldesigner.plugin.PluginUnitDefinition;
+
+import org.sbml.jsbml.JSBML;
 import org.sbml.jsbml.Parameter;
 import org.sbml.jsbml.util.TreeNodeChangeEvent;
 import org.sbml.jsbml.util.TreeNodeChangeListener;
@@ -30,37 +43,114 @@ import org.sbml.jsbml.util.TreeNodeChangeListener;
  * @date 10:50:22
  */
 public class PluginChangeListener implements TreeNodeChangeListener {
-	
-	/* (non-Javadoc)
-	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+
+	private CellDesignerPlugin plugin;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.
+	 * PropertyChangeEvent)
 	 */
 	public void propertyChange(PropertyChangeEvent event) {
+		Object eventsource = event.getSource();
+
 		String prop = event.getPropertyName();
 		if (prop.equals(TreeNodeChangeEvent.about)) {
-			
+
 		} else if (prop.equals(TreeNodeChangeEvent.addCVTerm)) {
-			
+
+		} else if (prop.equals(TreeNodeChangeEvent.addDeclaredNamespace)) {
+
 		}
 		// TODO Auto-generated method stub
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.sbml.jsbml.util.TreeNodeChangeListener#nodeAdded(javax.swing.tree.TreeNode)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.sbml.jsbml.util.TreeNodeChangeListener#nodeAdded(javax.swing.tree
+	 * .TreeNode)
 	 */
 	public void nodeAdded(TreeNode node) {
-		// TODO Auto-generated method stub
-		
+		if (node instanceof PluginCompartmentType) {
+			PluginCompartmentType pt = ((PluginCompartmentType) node);
+			plugin.notifySBaseAdded(pt);
+			
+		} else if (node instanceof PluginModel) {
+			PluginModel pt = (PluginModel) node;
+			plugin.notifySBaseAdded(pt);
+			
+		} else if (node instanceof PluginReaction) {
+			PluginReaction pt = (PluginReaction) node;
+			plugin.notifySBaseAdded(pt);
+			
+		} else if (node instanceof PluginSimpleSpeciesReference) {
+			PluginSimpleSpeciesReference pt = (PluginSimpleSpeciesReference) node;
+			plugin.notifySBaseAdded(pt);
+		} else if (node instanceof PluginSpeciesType) {
+			PluginSpeciesType pt = (PluginSpeciesType) node;
+			plugin.notifySBaseAdded(pt);
+		} else if (node instanceof PluginCompartment) {
+			PluginCompartment pt = (PluginCompartment) node;
+			plugin.notifySBaseAdded(pt);
+		} else if (node instanceof PluginParameter) {
+			PluginParameter pt = (PluginParameter) node;
+			plugin.notifySBaseAdded(pt);
+		} else if (node instanceof PluginSpecies) {
+			PluginSpecies pt = (PluginSpecies) node;
+			plugin.notifySBaseAdded(pt);
+		} else if (node instanceof PluginUnitDefinition) {
+			PluginUnitDefinition pt = (PluginUnitDefinition) node;
+			plugin.notifySBaseAdded(pt);
+		} else if (node instanceof PluginFunctionDefinition) {
+			PluginFunctionDefinition pt = (PluginFunctionDefinition) node;
+			plugin.notifySBaseAdded(pt);
+		}
+
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.sbml.jsbml.util.TreeNodeChangeListener#nodeRemoved(javax.swing.tree.TreeNode)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.sbml.jsbml.util.TreeNodeChangeListener#nodeRemoved(javax.swing.tree
+	 * .TreeNode)
 	 */
 	public void nodeRemoved(TreeNode node) {
-		if (node instanceof Parameter) {
+		if (node instanceof PluginCompartmentType) {
+			PluginCompartmentType pt = ((PluginCompartmentType) node);
+			plugin.notifySBaseDeleted(pt);
 			
+		} else if (node instanceof PluginModel) {
+			PluginModel pt = (PluginModel) node;
+			plugin.notifySBaseDeleted(pt);
+			
+		} else if (node instanceof PluginReaction) {
+			PluginReaction pt = (PluginReaction) node;
+			plugin.notifySBaseDeleted(pt);
+			
+		} else if (node instanceof PluginSimpleSpeciesReference) {
+			PluginSimpleSpeciesReference pt = (PluginSimpleSpeciesReference) node;
+			plugin.notifySBaseDeleted(pt);
+		} else if (node instanceof PluginSpeciesType) {
+			PluginSpeciesType pt = (PluginSpeciesType) node;
+			plugin.notifySBaseDeleted(pt);
+		} else if (node instanceof PluginCompartment) {
+			PluginCompartment pt = (PluginCompartment) node;
+			plugin.notifySBaseDeleted(pt);
+		} else if (node instanceof PluginParameter) {
+			PluginParameter pt = (PluginParameter) node;
+			plugin.notifySBaseDeleted(pt);
+		} else if (node instanceof PluginSpecies) {
+			PluginSpecies pt = (PluginSpecies) node;
+			plugin.notifySBaseDeleted(pt);
+		} else if (node instanceof PluginUnitDefinition) {
+			PluginUnitDefinition pt = (PluginUnitDefinition) node;
+			plugin.notifySBaseDeleted(pt);
+		} else if (node instanceof PluginFunctionDefinition) {
+			PluginFunctionDefinition pt = (PluginFunctionDefinition) node;
+			plugin.notifySBaseDeleted(pt);
 		}
-		// TODO Auto-generated method stub
-		
 	}
-	
 }
