9b7c5cce6fb0f55cfb2316743e49c34b03d12ee2
Maike Aichele <m.aichele@gmx.de>
2011-11-29 12:18:53 +0000

    nodeAdded filled with new if-branches

--- src/org/sbml/jsbml/xml/libsbml/LibSBMLChangeListener.java
+++ src/org/sbml/jsbml/xml/libsbml/LibSBMLChangeListener.java
@@ -20,12 +20,19 @@ import java.beans.PropertyChangeEvent;
 
 import javax.swing.tree.TreeNode;
 
+import org.sbml.jsbml.AbstractNamedSBase;
+import org.sbml.jsbml.AbstractSBase;
+import org.sbml.jsbml.Compartment;
 import org.sbml.jsbml.CompartmentType;
 import org.sbml.jsbml.Constraint;
 import org.sbml.jsbml.KineticLaw;
 import org.sbml.jsbml.MathContainer;
+import org.sbml.jsbml.NamedSBase;
+import org.sbml.jsbml.Parameter;
 import org.sbml.jsbml.Reaction;
+import org.sbml.jsbml.SBase;
 import org.sbml.jsbml.Species;
+import org.sbml.jsbml.UnitDefinition;
 import org.sbml.jsbml.util.TreeNodeChangeEvent;
 import org.sbml.jsbml.util.TreeNodeChangeListener;
 import org.sbml.libsbml.SBMLDocument;
@@ -54,25 +61,188 @@ public class LibSBMLChangeListener implements TreeNodeChangeListener {
 		// TODO: hash all elements without id. JSBML --> libSBML
 		// all kinds of Rules, initialAssignment, stoichiometryMath (but this is inside of reactions), constraint
 	}
-	
-	
+
+
 	/* (non-Javadoc)
 	 * @see org.sbml.jsbml.util.TreeNodeChangeListener#nodeAdded(javax.swing.tree.TreeNode)
 	 */
 	public void nodeAdded(TreeNode node) {
-		if (node instanceof CompartmentType) {
-			CompartmentType ct = (CompartmentType) node;
-			org.sbml.libsbml.CompartmentType libCt = libDoc.getModel().createCompartmentType();
-			if (ct.isSetId()) {
-				libCt.setId(ct.getId());
+		if (node instanceof SBase){
+			org.sbml.jsbml.AbstractSBase sbase = (AbstractSBase) node;
+			org.sbml.libsbml.SBase libSbase = (org.sbml.libsbml.SBase) new Object();
+			if (sbase.isSetAnnotation()){
+				libSbase.setAnnotation(sbase.getAnnotationString());
 			}
-			if (ct.isSetName()) {
-				libCt.setName(ct.getName());
+			if (sbase.isSetMetaId()){
+				libSbase.setMetaId(sbase.getMetaId());
 			}
-			// TODO: setSBOTerm etc.
-		}
-		// TODO Auto-generated method stub
+			if (sbase.isSetSBOTerm()){
+				libSbase.setSBOTerm(sbase.getSBOTerm());
+			}
+			if (sbase instanceof org.sbml.jsbml.NamedSBase) {
+				if (((org.sbml.jsbml.NamedSBase) sbase).isSetName()){
+					libSbase.setName(((org.sbml.jsbml.NamedSBase) sbase).getName());
+				}
+				if (sbase instanceof CompartmentType) {
+					CompartmentType ct = (CompartmentType) sbase;
+					org.sbml.libsbml.CompartmentType libCt = (org.sbml.libsbml.CompartmentType) libSbase;
+					if (ct.isSetId()) {
+						libCt.setId(ct.getId());
+					}
+					if (ct.isSetName()) {
+						libCt.setName(ct.getName());
+					}
+					if (ct.isSetSBOTerm()){
+						libCt.setSBOTerm(ct.getSBOTerm());
+					}
+					if (ct.isSetAnnotation()){
+						libCt.setAnnotation(ct.getAnnotationString());
+					}
+					if (ct.isSetMetaId()){
+						libCt.setMetaId(ct.getMetaId());
+					}
+					if (ct.isSetNotes()){
+						libCt.setNotes(ct.getNotesString());
+					}
+				} else if (node instanceof Compartment){
+					Compartment c = (Compartment) node;
+					org.sbml.libsbml.Compartment libC = libDoc.getModel().createCompartment();
+					if (c.isSetCompartmentType()){
+						libC.setCompartmentType(c.getCompartmentType());
+					}
+					if (c.isSetConstant()){
+						libC.setConstant(c.getConstant());
+					}
+					if (c.isSetId()){
+						libC.setId(c.getId());
+					}
+					if (c.isSetName()){
+						libC.setName(c.getName());
+					}
+					if (c.isSetOutside()){
+						libC.setOutside(c.getOutside());
+					}
+					if (c.isSetSize()){
+						libC.setSize(c.getSize());
+					}
+					if (c.isSetAnnotation()){
+						libC.setAnnotation(c.getAnnotationString());
+					}
+					if (c.isSetSpatialDimensions()){
+						libC.setSpatialDimensions(c.getSpatialDimensions());
+					}
+					if (c.isSetUnits()){
+						libC.setUnits(c.getUnits());
+					}
+					if (c.isSetSBOTerm()){
+						libC.setSBOTerm(c.getSBOTerm());
+					}
+					if (c.isSetVolume()){
+						libC.setVolume(c.getVolume());
+					}
+					if (c.isSetMetaId()){
+						libC.setMetaId(c.getMetaId());
+					}
+				} else if (node instanceof Species){
+					Species spec = (Species) node;
+					org.sbml.libsbml.Species libS = libDoc.getModel().createSpecies();
+					if (spec.isSetName()){
+						libS.setName(spec.getName());
+					}
+					if (spec.isSetAnnotation()){
+						libS.setAnnotation(spec.getAnnotationString());
+					}
+					if (spec.isSetBoundaryCondition()){
+						libS.setBoundaryCondition(spec.getBoundaryCondition());
+					}
+					if (spec.isSetCharge()){
+						libS.setCharge(spec.getCharge());
+					}
+					if (spec.isSetCompartment()){
+						libS.setCompartment(spec.getCompartment());
+					}
+					if (spec.isSetConstant()){
+						libS.setConstant(spec.getConstant());
+					}
+					if (spec.isSetConversionFactor()){
+						libS.setConversionFactor(spec.getConversionFactor());
+					}
+					if (spec.isSetHasOnlySubstanceUnits()){
+						libS.setHasOnlySubstanceUnits(spec.getHasOnlySubstanceUnits());
+					}
+					if (spec.isSetId()){
+						libS.setId(spec.getId());
+					}
+					if (spec.isSetInitialAmount()){
+						libS.setInitialAmount(spec.getInitialAmount());
+					}
+					if (spec.isSetInitialConcentration()){
+						libS.setInitialConcentration(spec.getInitialConcentration());
+					}
+					if (spec.isSetMetaId()){
+						libS.setMetaId(spec.getMetaId());
+					}
+					if (spec.isSetSBOTerm()){
+						libS.setSBOTerm(spec.getSBOTerm());
+					}
+					if (spec.isSetSpatialSizeUnits()){
+						libS.setSpatialSizeUnits(spec.getSpatialSizeUnits());
+					}
+					if (spec.isSetSpeciesType()){
+						libS.setSpeciesType(spec.getSpeciesType());
+					}
+					if (spec.isSetSubstanceUnits()){
+						libS.setSubstanceUnits(spec.getSubstanceUnits());
+					}
+					if (spec.isSetUnits()){
+						libS.setUnits(spec.getUnits());
+					}
+				} else if (node instanceof Parameter){
+					Parameter param = (Parameter) node;
+					org.sbml.libsbml.Parameter libParam = libDoc.getModel().createParameter();
+					if (param.isSetAnnotation()){
+						libParam.setAnnotation(param.getAnnotationString());
+					}
+					if (param.isSetConstant()){
+						libParam.setConstant(param.getConstant());
+					}
+					if (param.isSetId()){
+						libParam.setId(param.getId());
+					}
+					if (param.isSetMetaId()){
+						libParam.setMetaId(param.getMetaId());
+					}
+					if (param.isSetName()){
+						libParam.setName(param.getName());
+					}
+					if (param.isSetSBOTerm()){
+						libParam.setSBOTerm(param.getSBOTerm());
+					}
+					if (param.isSetUnits()){
+						libParam.setUnits(param.getUnits());
+					}
+					if (param.isSetValue()){
+						libParam.setValue(param.getValue());
+					}
+				} else if (node instanceof UnitDefinition){
+					UnitDefinition udef = (UnitDefinition) node;
+					org.sbml.libsbml.UnitDefinition libUdef = libDoc.getModel().createUnitDefinition();
+					if (udef.isSetAnnotation()){
+						libUdef.setAnnotation(udef.getAnnotationString());
+					}
+					if (udef.isSetMetaId()){
+						libUdef.setMetaId(udef.getMetaId());
+					}
+					if (udef.isSetName()){
+						libUdef.setName(udef.getName());
+					}
+					if (udef.isSetSBOTerm()){
 
+					}
+				}
+			}
+
+		} 
 	}
 
 	/* (non-Javadoc)
@@ -82,8 +252,8 @@ public class LibSBMLChangeListener implements TreeNodeChangeListener {
 		org.sbml.libsbml.Model libModel = libDoc.getModel();
 		if (node instanceof CompartmentType) {
 			CompartmentType ct = (CompartmentType) node;
-			org.sbml.libsbml.CompartmentType pt = libModel
-					.getCompartmentType(ct.getId());
+			//			org.sbml.libsbml.CompartmentType pt = libModel
+			//					.getCompartmentType(ct.getId());
 			libModel.removeCompartmentType(ct.getId());
 		}
 		// TODO Auto-generated method stub
@@ -92,27 +262,26 @@ public class LibSBMLChangeListener implements TreeNodeChangeListener {
 	/* (non-Javadoc)
 	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
 	 */
-	@SuppressWarnings("deprecation")
 	public void propertyChange(PropertyChangeEvent evt) {
 		Object evtSrc = evt.getSource();
 		String prop = evt.getPropertyName();
-		
+
 		// ...
 		if (prop.equals(TreeNodeChangeEvent.charge)) {
 			Species spec = (Species) evtSrc;
 			org.sbml.libsbml.Model libMod = libDoc.getModel();
 			libMod.getSpecies(spec.getId()).setCharge(spec.getCharge());
 		} else if (prop.equals(TreeNodeChangeEvent.math)) {
-		  MathContainer mathContainer = (MathContainer) evt.getSource();
-		  if (mathContainer instanceof KineticLaw) {
-			  Reaction r = ((KineticLaw) mathContainer).getParent();
-			  org.sbml.libsbml.KineticLaw libKl = libDoc.getModel().getReaction(r.getId()).getKineticLaw();
-			  libKl.setFormula(mathContainer.getFormula());
-		  } else if (mathContainer instanceof Constraint) {
-			  
-		  }
+			MathContainer mathContainer = (MathContainer) evt.getSource();
+			if (mathContainer instanceof KineticLaw) {
+				Reaction r = ((KineticLaw) mathContainer).getParent();
+				org.sbml.libsbml.KineticLaw libKl = libDoc.getModel().getReaction(r.getId()).getKineticLaw();
+				libKl.setFormula(mathContainer.getFormula());
+			} else if (mathContainer instanceof Constraint) {
+
+			}
 		}
-		
+
 		// TODO Auto-generated method stub
 
 	}
