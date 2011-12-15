52d637a4cecaa50968decfac4e59633bbd7ad509
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2011-12-15 12:08:25 +0000

    Some changes within the nodeAdded() Function, as well as the ASTNode Conversion Function.

--- src/org/sbml/jsbml/cdplugin/PluginChangeListener.java
+++ src/org/sbml/jsbml/cdplugin/PluginChangeListener.java
@@ -51,6 +51,7 @@ import jp.sbi.celldesigner.plugin.PluginUnitDefinition;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.sbml.jsbml.ASTNode;
 import org.sbml.jsbml.AlgebraicRule;
 import org.sbml.jsbml.Annotation;
 import org.sbml.jsbml.AssignmentRule;
@@ -86,8 +87,10 @@ import org.sbml.jsbml.Unit;
 import org.sbml.jsbml.UnitDefinition;
 import org.sbml.jsbml.util.TreeNodeChangeEvent;
 import org.sbml.jsbml.util.TreeNodeChangeListener;
+import org.sbml.libsbml.ListOfCompartments;
 import org.sbml.libsbml.SBase;
 import org.sbml.libsbml.XMLNode;
+import org.sbml.libsbml.libsbmlConstants;
 
 /**
  * @author Alexander Peltzer
@@ -306,13 +309,23 @@ public class PluginChangeListener implements TreeNodeChangeListener {
 		} else if (node instanceof RateRule) {
 			RateRule rule = (RateRule) node;
 			PluginRateRule plugraterule = new PluginRateRule(plugModel);
-			//PluginModel plugmodel = new PluginModel((Model) rule.getModel());
-			//PluginRateRule plugraterule = new PluginRateRule();
-			// TODO Why does this not work ?
+			plugraterule.setFormula(rule.getFormula());
+			plugraterule.setMath(convert(rule.getMath()));
+			plugraterule.setVariable(rule.getVariable());
+			//TODO Howto convert XMLNode elements from JSBML to libsbml ?
+			//This isn't as well described in the PluginSBMLWriter class
+			plugin.notifySBaseAdded(plugraterule);
+			
 		} else if (node instanceof AssignmentRule) {
 			AssignmentRule assignRule = (AssignmentRule) node;
-			// PluginAssignmentRule plugassignrule = new PluginAssignmentRule(assignRule.getModel());
-			// TODO Same problem here.
+			PluginAssignmentRule plugassignRule = new PluginAssignmentRule(plugModel);
+			plugassignRule.setFormula(assignRule.getFormula());
+			plugassignRule.setL1TypeCode(assignRule.getLevel());
+			plugassignRule.setMath(convert(assignRule.getMath()));
+			plugassignRule.setVariable(assignRule.getVariable());
+			//TODO Howto convert XMLNode elements from JSBML to libsbml ?
+			//This isn't as well described in the PluginSBMLWriter class
+			plugin.notifySBaseAdded(plugassignRule);
 		} else if (node instanceof KineticLaw) {
 			KineticLaw klaw = (KineticLaw) node;
 			Reaction parentreaction = klaw.getParentSBMLObject();
@@ -324,6 +337,9 @@ public class PluginChangeListener implements TreeNodeChangeListener {
 		} else if (node instanceof InitialAssignment) {
 			InitialAssignment iAssign = (InitialAssignment) node;
 			PluginInitialAssignment plugiassign = new PluginInitialAssignment(iAssign.getSymbol());
+			plugiassign.setMath(iAssign.getMathMLString());
+			//TODO Howto convert XMLNode elements from JSBML to libsbml ?
+			//This isn't as well described in the PluginSBMLWriter class
 			plugin.notifySBaseAdded(plugiassign);
 		} else if (node instanceof EventAssignment) {
 			EventAssignment eassign = (EventAssignment) node;
@@ -344,12 +360,17 @@ public class PluginChangeListener implements TreeNodeChangeListener {
 			plugin.notifySBaseAdded(plugrule);
 		} else if (node instanceof AlgebraicRule) {
 			AlgebraicRule alrule = (AlgebraicRule) node;
-			//PluginModel plugmodel = (PluginModel) alrule.getModel();
-			//PluginAlgebraicRule plugalrule = new PluginAlgebraicRule(alrule.getModel());
-			// TODO PluginModel Problem
+			PluginAlgebraicRule plugalrule = new PluginAlgebraicRule(plugModel);
+			plugalrule.setFormula(alrule.getFormula());
+			plugalrule.setMath(convert(alrule.getMath()));
+			//TODO Howto convert XMLNode elements from JSBML to libsbml ?
+			//This isn't as well described in the PluginSBMLWriter class
+			plugin.notifySBaseAdded(plugalrule);
 		} else if (node instanceof Constraint) {
 			Constraint ct = (Constraint) node;
 			PluginConstraint plugct = new PluginConstraint(ct.getMathMLString());
+			plugct.setMessage(ct.getMessageString());
+			//TODO XMLNode parsing as well here
 			plugin.notifySBaseAdded(plugct);
 		} else if (node instanceof Delay) {
 			Delay dl = (Delay) node;
@@ -374,6 +395,8 @@ public class PluginChangeListener implements TreeNodeChangeListener {
 			ListOf<?> listOf = (ListOf<?>) node;
 			switch (listOf.getSBaseListType()) {
 			  case listOfCompartments:
+				  ListOfCompartments ll = new ListOfCompartments();
+				  
 			    break;
 			  case listOfCompartmentTypes:
 			    break;
@@ -587,5 +610,278 @@ public class PluginChangeListener implements TreeNodeChangeListener {
 		} else if (node instanceof Creator){
 			//TODO
 		}
+	}
+	
+	/**
+	 * This stuff can be written separately into another class, maybe called utils; since it is used by both kind of plugins.
+	 * 
+	 */
+	
+	/**
+	 * 
+	 * @param ast
+	 * @return
+	 */
+	private org.sbml.libsbml.ASTNode convert(ASTNode astnode) {
+		org.sbml.libsbml.ASTNode libAstNode;
+		switch (astnode.getType()) {
+		case REAL:
+			libAstNode = new org.sbml.libsbml.ASTNode(libsbmlConstants.AST_REAL);
+			libAstNode.setValue(astnode.getReal());
+			break;
+		case INTEGER:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_INTEGER);
+			libAstNode.setValue(astnode.getInteger());
+			break;
+		case FUNCTION_LOG:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_LOG);
+			break;
+		case POWER:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_POWER);
+			break;
+		case PLUS:
+			libAstNode = new org.sbml.libsbml.ASTNode(libsbmlConstants.AST_PLUS);
+			break;
+		case MINUS:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_MINUS);
+			break;
+		case TIMES:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_TIMES);
+			break;
+		case DIVIDE:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_DIVIDE);
+			break;
+		case RATIONAL:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_RATIONAL);
+			break;
+		case NAME_TIME:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_NAME_TIME);
+			break;
+		case FUNCTION_DELAY:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_DELAY);
+			break;
+		case NAME:
+			libAstNode = new org.sbml.libsbml.ASTNode(libsbmlConstants.AST_NAME);
+			libAstNode.setName(astnode.getName());
+			break;
+		case CONSTANT_PI:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_CONSTANT_PI);
+			break;
+		case CONSTANT_E:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_CONSTANT_E);
+			break;
+		case CONSTANT_TRUE:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_CONSTANT_TRUE);
+			break;
+		case CONSTANT_FALSE:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_CONSTANT_FALSE);
+			break;
+		case REAL_E:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_REAL_E);
+			libAstNode.setValue(astnode.getMantissa(), astnode.getExponent());
+			break;
+		case FUNCTION_ABS:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ABS);
+			break;
+		case FUNCTION_ARCCOS:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCCOS);
+			break;
+		case FUNCTION_ARCCOSH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCCOSH);
+			break;
+		case FUNCTION_ARCCOT:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCCOT);
+			break;
+		case FUNCTION_ARCCOTH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCCOTH);
+			break;
+		case FUNCTION_ARCCSC:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCCSC);
+			break;
+		case FUNCTION_ARCCSCH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCCSCH);
+			break;
+		case FUNCTION_ARCSEC:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCSEC);
+			break;
+		case FUNCTION_ARCSECH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCSECH);
+			break;
+		case FUNCTION_ARCSIN:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCSIN);
+			break;
+		case FUNCTION_ARCSINH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCSINH);
+			break;
+		case FUNCTION_ARCTAN:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCTAN);
+			break;
+		case FUNCTION_ARCTANH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ARCTANH);
+			break;
+		case FUNCTION_CEILING:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_CEILING);
+			break;
+		case FUNCTION_COS:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_COS);
+			break;
+		case FUNCTION_COSH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_COSH);
+			break;
+		case FUNCTION_COT:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_COT);
+			break;
+		case FUNCTION_COTH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_COTH);
+			break;
+		case FUNCTION_CSC:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_CSC);
+			break;
+		case FUNCTION_CSCH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_CSCH);
+			break;
+		case FUNCTION_EXP:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_EXP);
+			break;
+		case FUNCTION_FACTORIAL:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_FACTORIAL);
+			break;
+		case FUNCTION_FLOOR:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_FLOOR);
+			break;
+		case FUNCTION_LN:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_LN);
+			break;
+		case FUNCTION_POWER:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_POWER);
+			break;
+		case FUNCTION_ROOT:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_ROOT);
+			break;
+		case FUNCTION_SEC:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_SEC);
+			break;
+		case FUNCTION_SECH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_SECH);
+			break;
+		case FUNCTION_SIN:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_SIN);
+			break;
+		case FUNCTION_SINH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_SINH);
+			break;
+		case FUNCTION_TAN:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_TAN);
+			break;
+		case FUNCTION_TANH:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_TANH);
+			break;
+		case FUNCTION:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION);
+			libAstNode.setName(astnode.getName());
+			break;
+		case LAMBDA:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_LAMBDA);
+			break;
+		case LOGICAL_AND:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_LOGICAL_AND);
+			break;
+		case LOGICAL_XOR:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_LOGICAL_XOR);
+			break;
+		case LOGICAL_OR:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_LOGICAL_OR);
+			break;
+		case LOGICAL_NOT:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_LOGICAL_NOT);
+			break;
+		case FUNCTION_PIECEWISE:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_FUNCTION_PIECEWISE);
+			break;
+		case RELATIONAL_EQ:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_RELATIONAL_EQ);
+			break;
+		case RELATIONAL_GEQ:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_RELATIONAL_GEQ);
+			break;
+		case RELATIONAL_GT:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_RELATIONAL_GT);
+			break;
+		case RELATIONAL_NEQ:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_RELATIONAL_NEQ);
+			break;
+		case RELATIONAL_LEQ:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_RELATIONAL_LEQ);
+			break;
+		case RELATIONAL_LT:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_RELATIONAL_LT);
+			break;
+		default:
+			libAstNode = new org.sbml.libsbml.ASTNode(
+					libsbmlConstants.AST_UNKNOWN);
+			break;
+		}
+		for (ASTNode child : astnode.getListOfNodes())
+			libAstNode.addChild(convert(child));
+		return libAstNode;
 	}
 }
