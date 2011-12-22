15851b68e1467bebd80f46630a6cf657d3844a67
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2011-12-22 12:44:49 +0000

    Created a testplugin to verify the functionality of the methods implemented for the CellDesigner Plugin.

---
+++ src/org/sbml/jsbml/cdplugin/test/CDPluginTester.java
@@ -0,0 +1,57 @@
+package org.sbml.jsbml.cdplugin.test;
+
+import jp.sbi.celldesigner.plugin.CellDesignerPlugin;
+import jp.sbi.celldesigner.plugin.PluginSBase;
+
+public class CDPluginTester extends CellDesignerPlugin {
+
+	/*
+	 * Constructors
+	 */
+	
+	public CDPluginTester(){
+		
+	}
+
+	@Override
+	public void SBaseAdded(PluginSBase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SBaseChanged(PluginSBase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SBaseDeleted(PluginSBase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addPluginMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modelClosed(PluginSBase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modelOpened(PluginSBase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modelSelectChanged(PluginSBase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
