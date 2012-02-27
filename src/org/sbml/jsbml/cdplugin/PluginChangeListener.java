5edd9725a0e7ee1c854ec3f2fdd9255688af9557
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2012-02-27 13:48:25 +0000

    Added a FIXME tag where there was a compile error.

--- src/org/sbml/jsbml/cdplugin/PluginChangeListener.java
+++ src/org/sbml/jsbml/cdplugin/PluginChangeListener.java
@@ -940,7 +940,8 @@ public class PluginChangeListener implements TreeNodeChangeListener {
 
 				switch (listOf.getSBaseListType()) {
 				case listOfCompartments:
-					ListOfCompartments ll = new ListOfCompartments();
+					// FIXME
+					//ListOfCompartments ll = new ListOfCompartments();
 					
 					break;
 				case listOfCompartmentTypes:
